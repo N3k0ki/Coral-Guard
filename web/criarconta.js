97e9c4e8b4fffe666f16d4c55d525f6cd203fd6b
Ana Clara! <129322817+N3k0ki@users.noreply.github.com>
2024-10-09 21:51:41 -0300

    Update criarconta.js

--- web/criarconta.js
+++ web/criarconta.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const senha = form["senha"].value;
 
       // Verificação para garantir que os valores estão sendo capturados
-      console.log(`Nome: ${nome}, Email: ${email}, Senha: ${senha}`);
+      alert(`Nome: ${nome}, Email: ${email}, Senha: ${senha}`);
 
       const dados = {
           nome: nome,
