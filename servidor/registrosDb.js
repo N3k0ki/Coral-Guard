c0b3b72c9a498a86ea219bdf93f44a5753cb1391
Ana Clara! <129322817+N3k0ki@users.noreply.github.com>
2024-10-26 17:50:13 -0300

    Delete servidor directory

--- servidor/registrosDb.js
+++
@@ -1,7 +0,0 @@
-import { registrosColecao } from "./dbConnect.js"
-
-function cadastrarUsuarios({ user, senha}){
-    return registrosColecao.insertOne({ user , senha })
-}
-
-export { cadastrarUsuarios };
