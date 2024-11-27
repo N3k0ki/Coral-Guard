249ac5edc4c7043889a2e15b81a0a2ce06fcf9e0
Ana Clara! <129322817+N3k0ki@users.noreply.github.com>
2024-11-27 16:10:57 -0300

    Update _routes.js

--- src/_routes.js
+++ src/_routes.js
@@ -1,10 +1,10 @@
-
 import { Home } from './pages/home/home';
 import Login from './pages/login/login';
 import { Register } from './pages/register/register';
 import { Terms } from './pages/term/terms';
 import { Inicio } from './pages/inicio/inicio'
 import { CoralForm } from './pages/addpost/addpost'
+import Biblioteca from './pages/biblioteca/biblioteca';
 
 export const routes = [
     { path: '/', element: <Home /> },
@@ -12,5 +12,6 @@ export const routes = [
     { path: '/login', element: <Login />},
     { path: '/terms', element: <Terms />},
     { path: '/inicio', element: <Inicio />},
-    { path: '/post', element: <CoralForm />}
+    { path: '/post', element: <CoralForm />},
+    { path: '/biblioteca', element: <Biblioteca />}
 ];
